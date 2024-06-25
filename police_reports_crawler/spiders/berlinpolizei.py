b8ddd289573b8353a78e447550c62c464fbd3e48
Samuel Tseng <samuel.tseng@homeday.de>
2024-06-25 21:33:06 +0200

    feat: enhance BerlinpolizeiSpider and implement BerlinpolizeiarchivSpider
    1. BerlinpolizeiSpider Enhancements:
    
    - Refactored to use ItemTransformer and PageItemSelector for structured data extraction.
    - Utilized SELECTOR_FOR_ALL and SELECTOR_FOR_NEXT_PAGE for cleaner CSS selection.
    - Improved the process of yielding items and following pagination.
    
    2. BerlinpolizeiarchivSpider Implementation:
    
    - Introduced a new spider, BerlinpolizeiarchivSpider, to handle the scraping of archived police reports from the Berlin police website.

--- police_reports_crawler/spiders/berlinpolizei.py
+++ police_reports_crawler/spiders/berlinpolizei.py
@@ -1,7 +1,7 @@
-import datetime
 import scrapy
 from police_reports_crawler.items import PoliceReportCase
-from police_reports_crawler.itemloaders import BerlinPoliceReportLoader
+from police_reports_crawler.itemloaders.berlin import ItemTransformer
+from police_reports_crawler.page_objects.berlin import PageItemSelector, SELECTOR_FOR_ALL, SELECTOR_FOR_NEXT_PAGE
 
 
 class BerlinpolizeiSpider(scrapy.Spider):
@@ -11,18 +11,25 @@ class BerlinpolizeiSpider(scrapy.Spider):
 
     def parse(self, response):
         
-        cases = response.css('#layout-grid__area--maincontent > section.modul-autoteaser > ul > li')
+        cases = response.css(SELECTOR_FOR_ALL)
 
         for case in cases:
-            case_item = BerlinPoliceReportLoader(item=PoliceReportCase(), selector=case)
-            case_item.add_css('created_at', 'div.cell.nowrap.date::text')
-            case_item.add_css('url', 'div.cell.text > a::attr(href)')
-            case_item.add_css('title', 'div.cell.text > a::text')
+            case_item = ItemTransformer(item=PoliceReportCase(), selector=case)
+            processer = PageItemSelector(itemloader=case_item)
 
-            yield case_item.load_item()
+            yield processer.to_item()
         
-        next_page = response.css('#layout-grid__area--maincontent > section.modul-autoteaser > nav > ul > li.pager-item-next > a::attr(href)').get()
+        next_page = response.css(SELECTOR_FOR_NEXT_PAGE).get()
         
         if next_page is not None:
            next_page_url = 'https://www.berlin.de' + next_page
            yield response.follow(next_page_url, callback=self.parse)
+
+
+class BerlinpolizeiarchivSpider(scrapy.Spider):
+    name = "berlinpolizeiarchiv"
+    allowed_domains = ["www.berlin.de"]
+    start_urls = ["https://www.berlin.de/polizei/polizeimeldungen/archiv/2024/"]
+
+    def parse(self, response):
+        pass
