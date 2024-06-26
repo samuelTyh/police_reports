ecccb66bc14a99c37413bbe08ff3d93374975020
Samuel Tseng <samuel.tseng@homeday.de>
2024-06-26 14:42:17 +0200

    fix: convert date strings to ISO format in ItemTransformer

--- police_reports_crawler/itemloaders/berlin.py
+++ police_reports_crawler/itemloaders/berlin.py
@@ -1,9 +1,19 @@
+import datetime
 from itemloaders.processors import TakeFirst, MapCompose
 from scrapy.loader import ItemLoader
+
+
+def text_to_datetime(date_string):
+    timestamp = datetime.datetime.strptime(date_string, "%d.%m.%Y %H:%M")
+    return timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+def remove_uhr(date_string):
+    return date_string.split(" Uhr")[0]
 
 
 class ItemTransformer(ItemLoader):
 
     default_output_processor = TakeFirst()
-    filed_at_in = MapCompose(lambda x: x.split(" Uhr")[0])
+
+    filed_at_in = MapCompose(remove_uhr, text_to_datetime)
     url_in = MapCompose(lambda x: 'https://www.berlin.de' + x )
