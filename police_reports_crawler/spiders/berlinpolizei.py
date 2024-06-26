f37cb49d9bd811ec44bb280191d7be8f6694cc4f
Samuel Tseng <samuel.tseng@homeday.de>
2024-06-26 14:46:24 +0200

    feat: add archive and proxy options to BerlinpolizeiSpider

--- police_reports_crawler/spiders/berlinpolizei.py
+++ police_reports_crawler/spiders/berlinpolizei.py
@@ -2,12 +2,32 @@ import scrapy
 from police_reports_crawler.items import PoliceReportCase
 from police_reports_crawler.itemloaders.berlin import ItemTransformer
 from police_reports_crawler.page_objects.berlin import PageItemSelector, SELECTOR_FOR_ALL, SELECTOR_FOR_NEXT_PAGE
+from police_reports_crawler.utils.utils import get_proxy_url
 
 
 class BerlinpolizeiSpider(scrapy.Spider):
     name = "berlinpolizei"
-    allowed_domains = ["www.berlin.de"]
     start_urls = ["https://www.berlin.de/polizei/polizeimeldungen/?page_at_1_6=1#headline_1_6"]
+    archive_urls = [
+        'https://www.berlin.de/polizei/polizeimeldungen/archiv/2024/', 
+        'https://www.berlin.de/polizei/polizeimeldungen/archiv/2023/', 
+        'https://www.berlin.de/polizei/polizeimeldungen/archiv/2022/', 
+        'https://www.berlin.de/polizei/polizeimeldungen/archiv/2021/', 
+        'https://www.berlin.de/polizei/polizeimeldungen/archiv/2020/'
+        ]
+    
+    def __init__(self, load_archive=False, with_proxy=False, *args, **kwargs):
+        super(BerlinpolizeiSpider, self).__init__(*args, **kwargs)
+        self.load_archive = load_archive == 'True'  # Convert string to boolean
+        self.with_proxy = with_proxy == 'True'
+        if self.load_archive:
+            self.start_urls.extend(self.archive_urls)
+        
+
+    def start_requests(self):
+        for start_url in self.start_urls:
+            url = get_proxy_url(start_url) if self.with_proxy else start_url
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         
@@ -24,12 +44,3 @@ class BerlinpolizeiSpider(scrapy.Spider):
         if next_page is not None:
            next_page_url = 'https://www.berlin.de' + next_page
            yield response.follow(next_page_url, callback=self.parse)
-
-
-class BerlinpolizeiarchivSpider(scrapy.Spider):
-    name = "berlinpolizeiarchiv"
-    allowed_domains = ["www.berlin.de"]
-    start_urls = ["https://www.berlin.de/polizei/polizeimeldungen/archiv/2024/"]
-
-    def parse(self, response):
-        pass
